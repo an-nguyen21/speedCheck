78a2f10169f049661c6973083f63a9443e895eb5
an-nguyen21 <anniengvyen1@gmail.com>
2021-01-11 21:19:46 -0600

    Fixed rounding error with for speeds over 70

--- speedChecker.py
+++ speedChecker.py
@@ -1,8 +1,11 @@
+import math
+
 speed = input("Speed: ")
 
 if int(speed) <= 70:
     print("Ok")
 
-elif int(speed) >= 70:
-    print("NO")
+elif int(speed) > 70:
+   demerit = (int(speed) - 70) / 5
+   print(f"Score: {math.ceil(demerit)}")
 
