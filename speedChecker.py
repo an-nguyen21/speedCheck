4e3be3ff7bfd835d8deac424248afda232d6e174
an-nguyen21 <anniengvyen1@gmail.com>
2021-01-11 21:41:59 -0600

    Added code for license suspended message Altered elif statement so it only goes up to 12 demerit points

--- speedChecker.py
+++ speedChecker.py
@@ -6,6 +6,10 @@ if int(speed) <= 70:
     print("Ok")
 
 elif int(speed) > 70:
-   demerit = (int(speed) - 70) / 5
-   print(f"Score: {math.ceil(demerit)}")
+   demerit = (int(speed) - 70) / 5 
+   if demerit < 12:
+        print(f"Score: {math.ceil(demerit)}")
+
+if int(demerit) > 12:
+    print("License suspended")
 
